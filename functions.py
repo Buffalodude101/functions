8234afc7709e580d939b520d105eee6a5af4b867
Buffalodude101 <haydenhart@mail.weber.edu>
2022-11-15 18:58:15 -0700

    get user length

---
+++ functions.py
@@ -0,0 +1,10 @@
+# conversion calculator
+# By: Hayden Hart
+
+# user input regarding the length to convert
+# get the unit from the user
+# convert the length to the correct unit
+# output the anwer to the user
+
+user_len = float(input("Enter a length to convert "))
+print(user_len)
