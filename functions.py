d5363930dd8375f727a29aadbdc64d0b8031b01c
Buffalodude101 <haydenhart@mail.weber.edu>
2022-11-17 18:31:46 -0700

    accept whole number and convert

--- functions.py
+++ functions.py
@@ -6,19 +6,37 @@
 # convert the length to the correct unit
 # output the anwer to the user
 
-user_len = float(input("Enter a length to convert "))
-user_unit = input("What unit is your length? ")
+while True:
+    while True:
+        user_len = (input("Enter a whole number to convert "))
+        if user_len.isdigit():
+            user_len = float(user_len)
+            break
+        else:
+            print("Please try agian")
+        
+        
+       
 
-# to convert inches to mm --> in X 25.4
-# to convert mm to inches --> mm / 25.4
 
-# user gives inches unit
-if user_unit == 'in':
-    convert_num = user_len * 25.4
-    convert_unit = 'mm'
-elif user_unit == 'mm':
-    convert_num = user_len / 25.4
-    convert_unit = 'in'
-else:
-    print("Invalid unit")
-print(f"{convert_num}{convert_unit}")
+
+    user_unit = input("What unit is your length? ")
+
+    # to convert inches to mm --> in X 25.4
+    # to convert mm to inches --> mm / 25.4
+
+    # user gives inches unit
+
+    if user_unit == 'in':
+        convert_num = user_len * 25.4
+        convert_unit = 'mm'
+        break
+    elif user_unit == 'mm':
+        convert_num = user_len / 25.4
+        convert_unit = 'in'
+        break
+    else:
+        print("Invalid unit")
+
+
+print(f"{convert_num:.2f}{convert_unit}")
